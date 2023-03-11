84f3b533d2d6fcdfaa6c553d7f73c61f223ccc74
Josiel Souza <josielsouza.dj@gmail.com>
2023-03-10 21:31:58 -0300

    Fix: change framework to vite

--- product/[id].tsx
+++
@@ -1,7 +0,0 @@
-import { useRouter } from "next/router";
-
-export default function Product() {
-  const { query } = useRouter();
-
-  return <h1>sucesso hgsdyatwytyhxbccyuw kkkk: {JSON.stringify(query)}</h1>;
-}
